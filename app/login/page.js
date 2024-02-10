39760a7edb5cdedadc5ad1abff40993e362cdce1
Premraj Patil <ppremraj697@gmail.com>
2024-02-11 01:11:33 +0530

    Styling Login Page

--- app/login/page.js
+++ app/login/page.js
@@ -8,7 +8,7 @@ async function Login() {
     const providers = await getProviders();
 
     return (
-        <div>
+        <div className='flex flex-col text-white bg-black min-h-screen w-full justify-center items-center'>
             {/* <img className='w-52 mb-5' src='https://links.papareact.com/9xl' /> */}
 
             <Image src="/spotifyLogo.png" alt="Spotify_Logo" width={208} height={500} style={{ marginBottom: '20px' }} />
@@ -17,7 +17,7 @@ async function Login() {
             {providers ? (
                 Object.values(providers).map((provider) => (
                     <div key={provider.name}>
-                        <button className='bg-[#18D860]'>Login with {provider.name}</button>
+                        <button className='bg-[#18D860] rounded-full p-5'>Login with {provider.name}</button>
                     </div>
                 ))
             ) : (
